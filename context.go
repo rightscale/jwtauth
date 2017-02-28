551ba4a00256fca1ba814cc8fd920ff4ee45deaf
Tony Spataro <tony@rightscale.com>
2017-02-28 14:22:27 -0800

    Use two contexts (one for each key) instead of an authInfo struct.

--- context.go
+++ context.go
@@ -7,37 +7,32 @@ import (
 type contextKey int
 
 const (
-	authInfoKey contextKey = iota + 1
+	claimsKey contextKey = iota + 1
+	tokenKey
 )
 
-// authInfo is used for storing claims & token in the context
-type authInfo struct {
-	claims Claims
-	token  string
-}
-
-// withAuthInfo creates a child context containing the given token & claims.
-func withAuthInfo(ctx context.Context, token string, claims Claims) context.Context {
-	return context.WithValue(ctx, authInfoKey, &authInfo{claims: claims, token: token})
-}
-
-// WithClaims creates a child context containing the given claims.
+// WithClaims creates a child context containing the given JWT claims.
 func WithClaims(ctx context.Context, claims Claims) context.Context {
-	return context.WithValue(ctx, authInfoKey, &authInfo{claims: claims})
+	return context.WithValue(ctx, claimsKey, claims)
 }
 
 // ContextClaims retrieves the JWT claims associated with the request.
 func ContextClaims(ctx context.Context) Claims {
-	if authInfo, _ := ctx.Value(authInfoKey).(*authInfo); authInfo != nil {
-		return authInfo.claims
+	if claims, _ := ctx.Value(claimsKey).(Claims); claims != nil {
+		return claims
 	}
 	return nil
 }
 
+// WithToken creates a child context containing the given JWT.
+func WithToken(ctx context.Context, token string) context.Context {
+	return context.WithValue(ctx, tokenKey, token)
+}
+
 // ContextToken retrieves the actual JWT associated with the request.
 func ContextToken(ctx context.Context) string {
-	if authInfo, _ := ctx.Value(authInfoKey).(*authInfo); authInfo != nil {
-		return authInfo.token
+	if token, _ := ctx.Value(tokenKey).(string); token != "" {
+		return token
 	}
 	return ""
 }
